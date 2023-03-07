52358f4bbc4f6d75891648d0a52e43c93a4d150f
Yash Gupta <yashgupta50357@gmail.com>
2023-03-07 22:40:54 +0530

    Chapter 2 completion

---
+++ src/Chapter-02/demo4.ts
@@ -0,0 +1,51 @@
+// https://stackoverflow.com/questions/57337598/in-typescript-what-do-extends-keyof-and-in-keyof-mean
+
+type ContactStatus = "active" | "inactive" | "new";
+
+export interface Address {
+    street: string;
+    province: string;
+    postalCode: string;
+}
+
+interface Contact {
+    id: number;
+    name: string;
+    status: ContactStatus;
+    address: Address;
+}
+
+type Awesome = Contact["address"]["postalCode"]
+
+interface ContactEvent {
+    contactId: Contact["id"];
+}
+
+interface ContactDeletedEvent extends ContactEvent {
+}
+
+interface ContactStatusChangedEvent extends ContactEvent {
+    oldStatus: Contact["status"];
+    newStatus: Contact["status"];
+}
+
+interface ContactEvents {
+    deleted: ContactDeletedEvent;
+    statusChanged: ContactStatusChangedEvent;
+    // ... and so on
+}
+
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+    return source[propertyName];
+}
+
+function handleEvent<T extends keyof ContactEvents>(
+    eventName: T,
+    handler: (evt: ContactEvents[T]) => void
+) {
+    if (eventName === "statusChanged") {
+        handler({ contactId: 1, oldStatus: "active", newStatus: "inactive" })
+    }
+}
+
+handleEvent("statusChanged", evt => evt)
